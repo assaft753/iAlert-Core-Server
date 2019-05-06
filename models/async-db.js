4ad06fa9ccfbb7e38f8cc3cb751206a2f9dac934
Sapir Kaplan <sapir@jpu.io>
2019-05-06 11:47:49 +0300

    update DB credential for TripleC server

--- models/async-db.js
+++ models/async-db.js
@@ -1,18 +1,18 @@
 var mysql = require('mysql');
 
-var pool = mysql.createPool({
-    host: "localhost",
-    user: "ialert",
-    password: "1234567",
-    database: 'iAlert'
-});
-
 // var pool = mysql.createPool({
 //     host: "localhost",
-//     user: "root",
-//     password: "ialert",
+//     user: "ialert",
+//     password: "1234567",
 //     database: 'iAlert'
 // });
+
+var pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "ialert",
+    database: 'iAlert'
+});
 
 module.exports = pool;
 
--- models/promise-db.js
+++ models/promise-db.js
@@ -1,16 +1,16 @@
 var mysql = require('sync-mysql');
 
-var pool = new mysql({
-    host: 'localhost',
-    user: 'ialert',
-    password: '1234567',
-    database: 'iAlert'
-});
-
 // var pool = new mysql({
 //     host: 'localhost',
-//     user: 'root',
-//     password: 'ialert',
+//     user: 'ialert',
+//     password: '1234567',
 //     database: 'iAlert'
-//   });
+// });
+
+var pool = new mysql({
+    host: 'localhost',
+    user: 'root',
+    password: 'ialert',
+    database: 'iAlert'
+  });
 module.exports = pool;
